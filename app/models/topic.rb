68b5cdd2d9867def1e923394eb5dc9f44ad82b45
razvanmiclau <razvan.miclau@gmail.com>
2015-11-13 14:53:33 +0000

    Added topics with questions
    ! Issues to fix !

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,4 @@
 class Topic < ActiveRecord::Base
+  validates_presence_of :title
+  has_many :questions
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113123925) do
+ActiveRecord::Schema.define(version: 20151113131225) do
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.string   "type"
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "questions", ["topic_id"], name: "index_questions_on_topic_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
