b85de8c4e5aa1b23b197bab68dfc868551fdf152
razvanmiclau <razvan.miclau@gmail.com>
2015-11-13 13:10:21 +0000

    Sidebar Nav

--- app/assets/javascripts/topics.js
+++ app/assets/javascripts/topics.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    
